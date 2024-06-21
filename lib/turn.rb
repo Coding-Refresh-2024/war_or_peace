4538e484724dca97932c6a33687f219b3922a5c6
Cory Bethune <corybbethune@gmail.com>
2024-06-21 11:12:30 -0700

    Created turn spec test variables and initialize/attribute test.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,7 @@
 class Turn
 
+  attr_reader :player1, :player2
+
+  
 
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -7,14 +7,31 @@ require 'pry'
 
 RSpec.describe Turn do
   before :each do
-    @card1 = Card.new(:diamond, 'Queen', 12)
-    @card2 = Card.new(:spade, '3', 3)
-    @card3 = Card.new(:heart, 'Ace', 14)
-    @card4 = Card.new(:club, '5', 5)
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
 
-    @cards = [@card1, @card2, @card3]
+    @cards1 = [card1, card2, card5, card8]
+    @cards2 = [card3, card4, card6, card7]
 
-    @deck = Deck.new(@cards)
+    @deck1 = Deck.new(@cards1)
+    @deck2 = Deck.new(@cards2)
 
-    @player = Player.new('Clarisa', @deck)
+    @player1 = Player.new('Megan', @deck1)
+    @player2 = Player.new('Aurora', @deck2)
+
+    @turn = Turn.new(@player1, @player2)
   end
+
+  it 'exists and has readable attributes' do
+    expect(@turn).to be_an_instance_of(Turn)
+    expect(@turn.player1).to eq(@player1)
+    expect(@turn.player2).to eq(@player2)
+  end
+
+end
