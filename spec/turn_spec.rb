9298dc7b9c84eb530ed665662578550d837b03db
Cory Bethune <corybbethune@gmail.com>
2024-06-21 12:41:10 -0700

    Created test for award_spoils turn class method.

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -98,4 +98,24 @@ RSpec.describe Turn do
     expect(@turn.player1.deck.cards.count).to eq(1)
     expect(@turn.player2.deck.cards.count).to eq(0)
   end
+
+  it "awards the spoils_of_war to the current turn winner" do
+    expect(@turn.spoils_of_war).to eq([])
+    expect(@turn.player1.deck.cards.count).to eq(4)
+    expect(@turn.player2.deck.cards.count).to eq(4)
+    expect(@turn.type).to eq(:basic)
+
+    winner = @turn.winner
+    @turn.pile_cards
+
+    expect(@turn.spoils_of_war).to eq([@card1, @card3])
+    expect(@turn.player1.deck.cards.count).to eq(3)
+    expect(@turn.player2.deck.cards.count).to eq(3)
+
+    @turn.award_spoils(winner)
+
+    expect(@turn.spoils_of_war).to eq([])
+    expect(@turn.player1.deck.cards.count).to eq(5)
+    expect(@turn.player2.deck.cards.count).to eq(3)
+  end
 end
