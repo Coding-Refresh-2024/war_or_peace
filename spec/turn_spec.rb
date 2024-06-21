a5c2e91aeb92b20a62bfa9ec4a06c9601955e8d3
Cory Bethune <corybbethune@gmail.com>
2024-06-21 11:00:41 -0700

    Created Turn class and test files.

---
+++ spec/turn_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require 'pry'
+
+RSpec.describe Turn do
+  before :each do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @card4 = Card.new(:club, '5', 5)
+
+    @cards = [@card1, @card2, @card3]
+
+    @deck = Deck.new(@cards)
+
+    @player = Player.new('Clarisa', @deck)
+  end
