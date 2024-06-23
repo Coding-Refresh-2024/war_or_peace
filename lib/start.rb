dfedbc51976e7784e3c55566042c6ca4bf0720a8
Cory Bethune <corybbethune@gmail.com>
2024-06-23 12:57:44 -0700

    Added game logic to account for player victory and draw conditions.

--- lib/start.rb
+++ lib/start.rb
@@ -16,31 +16,44 @@ class Start
 
     elsif @input == 'GO'
 
-      while turn_count < 1000000 && turn.player1.has_lost? == false && turn.player2.has_lost? == false
-        if turn.type == :basic
+      while turn_count < 1000001 && turn.player1.has_lost? == false && turn.player2.has_lost? == false
+        if turn_count == 1000000
+          
+          p "---- DRAW ----"
 
-          p "Turn #{turn_count}: #{turn.winner.name} won 2 cards"
+        elsif  turn.player1.has_lost? == true
 
-          turn.pile_cards
-          turn.award_spoils(turn.winner)
-          turn_count += 1
+          p "*~*~*~* #{turn.player2.name} has won the game! *~*~*~*"
 
-        elsif turn.type == :war
+        elsif turn.player2.has_lost? == true
 
-          p "Turn #{turn_count}: WAR - #{turn.winner.name} won 6 cards"
+          p "*~*~*~* #{turn.player1.name} has won the game! *~*~*~*"
 
-          turn.pile_cards
-          turn.award_spoils(turn.winner)
-          turn_count += 1
+        else
+          if turn.type == :basic
 
-        elsif turn.type == :mutually_assured_destruction
+            p "Turn #{turn_count}: #{turn.winner.name} won 2 cards"
 
-          p "Turn #{turn_count}: *mutually assured destruction* 6 cards removed from play"
+            turn.pile_cards
+            turn.award_spoils(turn.winner)
+            turn_count += 1
 
-          turn.pile_cards
-          turn_count += 1
+          elsif turn.type == :war
 
-        end
+            p "Turn #{turn_count}: WAR - #{turn.winner.name} won 6 cards"
+
+            turn.pile_cards
+            turn.award_spoils(turn.winner)
+            turn_count += 1
+
+          elsif turn.type == :mutually_assured_destruction
+
+            p "Turn #{turn_count}: *mutually assured destruction* 6 cards removed from play"
+
+            turn.pile_cards
+            turn_count += 1
+
+          end
 
       end
 
